4d36583a997e60282403e02d13eccfe4a778be48
Saurav Bharti <sauravbhartibgs37@gmail.com>
2023-06-19 20:51:16 +0530

    first commit

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useAuth } from '../context/AuthContext'
+import { Navigate } from 'react-router-dom'
+
+export default function PrivateRoute({children}) {
+    const {currentUser} = useAuth()
+
+    return currentUser? children : <Navigate to="/login" />
+}
